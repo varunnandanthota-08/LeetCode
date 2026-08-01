a61676baa6a51bf55b333edc1c1388b900bec04b
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 01:01:21 +0530

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/2/2026, 1:01:21 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root){
+            if(val==root->val){
+                return root;
+            }
+            else if(val<root->val){
+                root=root->left;
+            }
+            else{
+                root=root->right;
+            }
+        }
+        return NULL;
+    }
+};
