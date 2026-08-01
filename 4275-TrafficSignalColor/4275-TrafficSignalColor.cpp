b7f207bb8e06c9bf0cf84f5d55b48627b8b07e1a
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 00:53:22 +0530

    Create 4275-TrafficSignalColor

---
+++ 4275-TrafficSignalColor/4275-TrafficSignalColor.cpp
@@ -0,0 +1,11 @@
+// Last updated: 8/2/2026, 12:53:22 AM
+class Solution {
+public:
+    string trafficSignal(int timer) {
+        if(timer==0)return "Green";
+        else if(timer==30)return "Orange";
+        else if(timer>30&&timer<=90) return "Red";
+        return "Invalid";
+
+    }
+};
