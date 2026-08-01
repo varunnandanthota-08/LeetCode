0daeb794a78e2d3201d4e49446d767985db60d40
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 01:01:34 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.cpp
@@ -0,0 +1,32 @@
+// Last updated: 8/2/2026, 1:01:35 AM
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int>st;
+        for(string op:operations)
+        {
+            if(op=="C"){
+                st.pop();
+            }
+            else if(op=="D"){
+                st.push(2*st.top());
+            }
+            else if(op=="+"){
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.push(a);
+                st.push(a+b);
+            }
+            else{
+                st.push(stoi(op));
+            }
+        }
+        int sum=0;
+        while(!st.empty()){
+            sum+=st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};
