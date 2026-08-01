67965e71a49895dc44ec3a26b85c27e46f6d6de6
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 01:01:45 +0530

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/2/2026, 1:01:45 AM
+class Solution {
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
