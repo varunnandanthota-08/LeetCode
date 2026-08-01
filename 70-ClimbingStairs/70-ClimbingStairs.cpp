fbc758800613b99cd5a3bc1e892ae2a09f871135
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 01:04:32 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/2/2026, 1:04:32 AM
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
