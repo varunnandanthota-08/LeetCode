b12719de1131bfd66de9440d5860ca24508c67c5
varunnandanthota-08 <varunnandanthota@gmail.com>
2026-08-02 00:55:05 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ 3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/2/2026, 12:55:05 AM
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+//n2,n2+n
+//gcd(n2,n2+n)
+//gcd(n2,(n2+n)-n2)
+//gcd(n2,n)
+//n*gcd(n,1)
+//n*1
+//n
